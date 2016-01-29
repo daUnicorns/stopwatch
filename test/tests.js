183a21b7db3b96c234334875cc1e56ac7b8fcbad
Azureaus <liam.oshea@o2-v2.com>
2016-01-29 11:18:44 +0000

    tried to refactor code to work with test page and take some elements out of the T object

--- test/tests.js
+++ test/tests.js
@@ -1,29 +1,36 @@
-test( "timeElapsed should be Zero before we start the Timer", function() {
-  equal( T.timeElapsed, 0, 'Message: timer is zero at start' );
+test( "Check that time is returned as a stopwatch string with always double digits e.g. input->9 output->09", function() {
+  equal( T.humanTime(0), '00' );
+  equal( T.humanTime(1), '01' );
+  equal( T.humanTime(10), '10' );
 });
 
-test( "startTimer() starts counting from *NOW* (when instructed)", function() {
-    var startTime = new Date().getTime();
-    equal( T.startTimer(startTime), startTime, true );
-});
 
-test("stopTimer() stops counting from NOW", function() {
-    var stopTime = new Date().getTime();
-    equal( T.stopTimer(stopTime), stopTime, true);
-});
-
-test("elapsedTime() calculates the difference between stop and startTime", function() {
-    var stopTime = 1234;
-    var startTime = 345;
-    var timePassed = stopTime - startTime;
-    equal( T.timeDiff(startTime,stopTime), timePassed, true);
-});
-
-test("humanTime() takes millseconds and returns a human readable format of that e.g. 00:00:00:00", function() {
-    var mSecs = 10010; /*00:00:10:01*/
-    var mSecs2 = 100760; /*00:01:40:76*/
-    var mSecs3 = 12600000; /*03:30:00:00 */
-    equal(T.humanTime(12600000), '03:30:00:00',true);
-    equal(T.humanTime(100760), '00:01:40:76',true);
-    equal(T.humanTime(10010), '00:00:10:01',true);
-});
+// test( "timeElapsed should be Zero before we start the Timer", function() {
+//   equal( T.timeElapsed, 0, 'Message: timer is zero at start' );
+// });
+//
+// test( "startTimer() starts counting from *NOW* (when instructed)", function() {
+//     var startTime = new Date().getTime();
+//     equal( T.startTimer(startTime), startTime, true );
+// });
+//
+// test("stopTimer() stops counting from NOW", function() {
+//     var stopTime = new Date().getTime();
+//     equal( T.stopTimer(stopTime), stopTime, true);
+// });
+//
+// test("elapsedTime() calculates the difference between stop and startTime", function() {
+//     var stopTime = 1234;
+//     var startTime = 345;
+//     var timePassed = stopTime - startTime;
+//     equal( T.timeDiff(startTime,stopTime), timePassed, true);
+// });
+//
+// test("humanTime() takes millseconds and returns a human readable format of that e.g. 00:00:00:00", function() {
+//     var mSecs = 10010; /*00:00:10:01*/
+//     var mSecs2 = 100760; /*00:01:40:76*/
+//     var mSecs3 = 12600000; /*03:30:00:00 */
+//     equal(T.humanTime(12600000), '03:30:00:00',true);
+//     equal(T.humanTime(100760), '00:01:40:76',true);
+//     equal(T.humanTime(10010), '00:00:10:01',true);
+// });
